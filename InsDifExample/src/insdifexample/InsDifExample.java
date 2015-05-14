22c5fcdb8a0677b36062193138f5b42409880e34
George Liaros <liarosge@gmail.com>
2015-05-14 14:48:45 +0300

    2

--- InsDifExample/src/insdifexample/InsDifExample.java
+++ InsDifExample/src/insdifexample/InsDifExample.java
@@ -13,15 +13,18 @@ import weka.core.Utils;
  * @author geol
  */
 public class InsDifExample {
-    public static final String TRAIN_FILEPATH = "C:\\Users\\geol\\Dropbox\\advanced ml\\data\\testData\\emotions-train.arff";
-    public static final String XML_FILEPATH = "C:\\Users\\geol\\Dropbox\\advanced ml\\data\\testData\\emotions.xml";
+    public static String trainFilePath;// = "C:\\Users\\geol\\Dropbox\\advanced ml\\data\\testData\\emotions-train.arff";
+    public static String testFilePath;
+    public static String xmlFilePath;// = "C:\\Users\\geol\\Dropbox\\advanced ml\\data\\testData\\emotions.xml";
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) throws Exception {
-        String arffFilename = Utils.getOption("arff", args);
-        String xmlFilename = Utils.getOption("xml", args);
-        MultiLabelInstances dataset = new MultiLabelInstances(TRAIN_FILEPATH, XML_FILEPATH);
+        ClassLoader classLoader = new InsDifExample().getClass().getClassLoader();
+        trainFilePath = classLoader.getResource("resources/emotions-train.arrf").getFile();
+        testFilePath = classLoader.getResource("resources/emotions-test.arff").getFile();
+        xmlFilePath = classLoader.getResource("resources/emotions.xml").getFile();
+        MultiLabelInstances dataset = new MultiLabelInstances(trainFilePath, xmlFilePath);
         InsDif ins = new InsDif();
         ins.buildInternal(dataset);
     }
